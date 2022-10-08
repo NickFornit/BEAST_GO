dbae4ee28dbbbc1ee5851a34dc44495cd3549b45
PalarmAlex <instrpro@mail.ru>
2022-10-08 19:05:06 +0600

    марафет brain.go

--- brain/brain.go
+++ brain/brain.go
@@ -1,7 +1,4 @@
-/*  общие глобальные дела
-
-нейросеть
- */
+/* общие глобальные дела */
 package brain
 
 import (
@@ -13,42 +10,34 @@ import (
 	TerminalActions "BOT/brain/terminete_action"
 )
 
-///////////////////////////////////////////////////////////////////////////
-var IsStressing=0;// не успевает за 1 сек отсканировать
-var isWorking=0
-
-
+var IsStressing = 0	// не успевает за 1 сек отсканировать
+var isWorking = 0 	// флаг блока обработки нейросети
 
 /* Главный цикл (пульс) опроса состояния Beast и его ответов
 активируется каждую секунду из neurons.go
- */
-
-func Аctivation(lifeTime int){
+*/
+func Аctivation(lifeTime int) {
 	// true - смерть Beast при повреждении >99%
-	if gomeostas.IsBeastDeath{// прекращение раздачи пульса
+	if gomeostas.IsBeastDeath { // прекращение раздачи пульса
 		return
 	}
-	if isWorking==1 {
-		IsStressing=1
+	if isWorking == 1 {
+		IsStressing = 1
 		return
-	}else {
-		IsStressing=0
+	} else {
+		IsStressing = 0
 	}
 	// начало обработки нейросети
-	isWorking=1
-
-	isSlipping:=sleep.GetSleepCondition()
-
+	isWorking = 1
+	isSlipping := sleep.GetSleepCondition()
 	// текущее состояние гомеостаза и базового контекста с каждым пульсом
-	gomeostas.GomeostazPuls(EvolushnStage,lifeTime,PulsCount,isSlipping)
-	action_sensor.ActionSensorPuls(EvolushnStage,lifeTime,PulsCount,isSlipping)
-	sleep.SleepPuls(EvolushnStage,lifeTime,PulsCount)
-	reflexes.ReflexCountPuls(EvolushnStage,lifeTime,PulsCount,isSlipping)
-	TerminalActions.TermineteActionCountPuls(EvolushnStage,lifeTime,PulsCount,isSlipping)
-	psychic.PsychicCountPuls(EvolushnStage,lifeTime,PulsCount,isSlipping)
+	gomeostas.GomeostazPuls(EvolushnStage, lifeTime, PulsCount, isSlipping)
+	action_sensor.ActionSensorPuls(EvolushnStage, lifeTime, PulsCount, isSlipping)
+	sleep.SleepPuls(EvolushnStage, lifeTime, PulsCount)
+	reflexes.ReflexCountPuls(EvolushnStage, lifeTime, PulsCount, isSlipping)
+	TerminalActions.TermineteActionCountPuls(EvolushnStage, lifeTime, PulsCount, isSlipping)
+	psychic.PsychicCountPuls(EvolushnStage, lifeTime, PulsCount, isSlipping)
 	psychic.SetNotAllowAnyActions(NotAllowAnyActions)
-
 	// конец обработки нейросети
-	isWorking=0
+	isWorking = 0
 }
-
